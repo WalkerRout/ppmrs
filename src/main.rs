dab5d331d15f7b4eeb10752e845b39c5a64cc37d
walkerrout04@gmail.com <walkerrout04@gmail.com>
2022-11-22 14:34:04 -0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod ppmimage;
 
 use ppmimage::PPMImage;
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
   let mut image = PPMImage::new(680, 480);
   image.fill(0xFF0000FF);
   image.save("testrust.ppm")?;
